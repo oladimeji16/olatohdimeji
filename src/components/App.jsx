328d4ea9b2c861b8ad7bd5e3a164b18805bf1d8f
Oladimeji Mustapha <49591168+oladimeji16@users.noreply.github.com>
2020-10-12 19:01:34 +0100

    Made Nav Bar component, currently working on button

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import NavBar from './navbar/NavBar'
+
+function App() {
+    return <div className= 'App'>
+        <NavBar />
+        <h1>Hello World</h1>
+    </div>
+}
+
+
+
+
+
+
+
+
+
+export default App
---
+++ src/components/navbar/MenuItems.jsx
@@ -0,0 +1,25 @@
+const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'PortFolio',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
+
+
+export default MenuItems
