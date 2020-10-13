70eec263afdc5cd51f96051a76a75da32d62da59
Oladimeji Mustapha <49591168+oladimeji16@users.noreply.github.com>
2020-10-13 12:44:46 +0100

    Finished Landing Page, remains responsiveness

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import NavBar from './navbar/NavBar'
-import {Button} from './button/Button'
+import Landing from './Landing/Landing'
 
 function App() {
     return <div className= 'App'>
         <NavBar />
-        <h1>Hello World</h1>
-        <Button>Learn More</Button>
+        <Landing />
+        
     </div>
 }
 
