27de0294caef496535ab136800b60f1e017194d5
Oladimeji Mustapha <49591168+oladimeji16@users.noreply.github.com>
2020-10-15 00:36:28 +0100

    Finished About section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { Button } from '../button/Button'
-
+import './About.css'
 function About() {
     return <section className="about">
         <div className="about-text">
-            <h2>About</h2>
-            <p>I am a Nigerian based UI/UX Designer, Front-end and Mobile Developer.</p>
-            <p>For the past few years I have been building awesome digital projects mostly for myself 😉 and clients, I am a goal-driven person who ensures client satisfaction while advising them on the proper step to take to achieve more productivity in their businesses.</p>
-        </div>
-        <Button>Contact Me</Button>
-        <div className="skills">
-            <h2>Skills</h2>
+            <h2 className='heading'>About</h2>            
+            {/* <p><hr/></p> */}
+            <p className='para1'>I am a Nigerian based UI/UX Designer, Front-end and Mobile Developer.</p>
+            <p className='para2'>For the past few years I have been building awesome digital projects mostly for myself 😉 and clients, I am a goal-driven person who ensures client satisfaction while advising them on the proper step to take to achieve more productivity in their businesses.</p>
+            <Button className='btn'>Contact Me</Button>
+        </div>    
+        <div className="skills-part">
+            <h2 className='skills'>Skills</h2>
+            <p className='skills-p'>CSS</p>
+            <p className='skills-p'>Figma(Design Tool)</p>
+            <p className='skills-p'>Firebase</p>
+            <p className='skills-p'>Flutter</p>
+            <p className='skills-p'>HTML</p>
+            <p className='skills-p'>Javascript(ES6)</p>
+            <p className='skills-p'>React.js</p>
         </div>
     </section>
     
