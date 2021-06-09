58d8e765629fef7b9556625ec2cc3f695d6c6b69
Oladimeji Mustapha <49591168+oladimeji16@users.noreply.github.com>
2021-06-09 21:29:31 +0800

    Added dynamic year

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,10 +3,12 @@ import './Footer.css'
 
 
 function Footer() {
+    let d = new Date();
+    
     return <footer className='footer'>
         <p className='foo'> Every Inch designed and developed by me <a href="mailto:olatohdimeji16@gmail.com" className='twit'>Mustapha Abdullah</a></p>
-        <p className="foo">"Every of your dream, can come to fuition"</p>
-        <p className='foo'> &copy; 2020, MA</p>
+        <p className="foo">"Every of your dream can come to fuition"</p>
+        <p className='foo'> &copy; {d.getFullYear()}, MA</p>
 
     </footer>
     
